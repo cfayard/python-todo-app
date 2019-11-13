a4162c4de2a026328e557d06be420d4dbbcac75b
Casey Fayard <TerminusX@Caseys-MacBook-Pro.local>
2019-11-13 15:23:50 -0500

    adds print to-do function

--- todo.py
+++ todo.py
@@ -1,17 +1,23 @@
 # keep track of todos using a list
 todo_list = []
 
+# I need to print my todos
+
+def print_todos():
+    for todo in todo_list:
+        print(todo)
+
 # I need a way to add todos
 def add_todo(todo):
     # We receive a todo, which is a string
     todo_list.append(todo)
 
 # print the empty todo list
-print(todo_list)
+print_todos()
 # add a todo by calling our function
 add_todo("feed the cat")
 # print the todo list again, making sure
-print(todo_list)
+print_todos()
 
 # I need to be able to delete todos
 def delete_todo(index):
@@ -20,11 +26,12 @@ def delete_todo(index):
         todo_list.pop(index)
     except IndexError:
         print("👎👎 Sorry, we couldn't find that one.")
+
 delete_todo(0)
-print(todo_list)
+print_todos()
 delete_todo(0)
-print(todo_list)
-# I need to print my todos
+print_todos()
+
 
 # Show user main menu
 
